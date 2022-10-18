73233eeb76dbb756cd792262d0ea9593b8c666e6
Marco Alonso AP <mrodriguez@alquimiapay.com>
2022-10-18 11:14:11 -0500

    animations added

--- LottieAnimationDemo/ViewController.swift
+++ LottieAnimationDemo/ViewController.swift
@@ -10,13 +10,43 @@ import Lottie
 
 class ViewController: UIViewController {
     
+    let animationView = AnimationView()
     
+    var namesAnimations = ["clouds", "mapnav", "mappin", "area", "location", "world"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let wichAnimation = Int.random(in: 0..<6)
+        setupAnimation(index: wichAnimation)
+        
+        Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(changeAnimation), userInfo: nil, repeats: false)
+        
+        
+    }
+    
+    @objc func changeAnimation() {
+        let wichAnimation = Int.random(in: 0..<6)
+        setupAnimation(index: wichAnimation)
+    }
+    
+    @objc func changeScreen() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        vc.modalPresentationStyle = .fullScreen
+        vc.modalTransitionStyle = .crossDissolve
+        self.present(vc, animated: true)
     }
 
+    func setupAnimation(index: Int){
+        animationView.animation = Animation.named(namesAnimations[index])
+        animationView.frame = view.bounds
+        animationView.backgroundColor = .white
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        view.addSubview(animationView)
+    }
 
 }
 
