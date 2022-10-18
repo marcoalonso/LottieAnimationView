8b591ebb4f2ec5a93e1c7aa50e6342dddea8ec3b
Marco Alonso AP <mrodriguez@alquimiapay.com>
2022-10-18 10:57:46 -0500

    First Commit

---
+++ LottieAnimationDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  LottieAnimationDemo
+//
+//  Created by marco rodriguez on 18/10/22.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
